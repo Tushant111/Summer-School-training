287cb1345ad6cb122bb9de45aaca14dd4c1b2f77
Tushant karihaloo <101183687+Tushant111@users.noreply.github.com>
2024-07-30 15:37:11 +0530

    Update Reverse the String.cpp

--- Reverse the String.cpp
+++ Reverse the String.cpp
@@ -40,16 +40,12 @@
 
 
 
-
-
 #include <bits/stdc++.h> 
 string reverseString(string str)
 {
-	int n=str.length();
-	for(int i=0;i<n/2;i++){
-		swap(str[i], str[n-i-1]);
-	}
+	reverse(str.begin(),str.end());
 	return str;
+	
 }
 
 
